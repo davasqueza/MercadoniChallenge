cb09b536b2eafeccc62a84522ed591efae6427d9
Diego Andres Vasquez Avila <davasqueza@unal.edu.co>
2017-06-17 00:08:55 -0500

    Implemented address geocode and marker render

--- src/app/main/landing/landing.controller.js
+++ src/app/main/landing/landing.controller.js
@@ -6,9 +6,13 @@
     .controller('LandingController', LandingController);
 
   /** @ngInject */
-  function LandingController() {
+  function LandingController(LandingService, _,$log, uiGmapGoogleMapApi) {
     var vm = this;
+    vm.map = {};
 
+    /*
+    * initMap: Initialize basic map configuration
+    * */
     function initMap() {
       vm.mapConfig = {
         center: {
@@ -18,6 +22,66 @@
       };
     }
 
+
+    /*
+    * loadDirections: Load directions list to be processed
+    * */
+    function loadDirections() {
+      LandingService.getDirectionsData()
+        .then(function (data) {
+          geocodeDirections(data.content);
+        })
+        .catch(function (error) {
+          $log.log("Error:", error);
+        });
+    }
+
+    /*
+    * geocodeDirections: Process the directions list to obtain places data
+    * */
+    function geocodeDirections(directions) {
+      LandingService.geocodeAddressList(directions)
+        .then(function (locations) {
+          processLocationsData(locations);
+          markersBoundingBox(locations);
+        })
+        .catch(function (error) {
+          $log.log("Error:", error);
+        });
+    }
+
+    /*
+    * processLocationsData: Process places data to obtain data usable on markers
+    * */
+    function processLocationsData(locations) {
+      vm.locations = _.map(locations, function (location) {
+        var place = location.results[0];
+        var marker = {
+          coords: {
+            latitude: place.geometry.location.lat,
+            longitude: place.geometry.location.lng
+          },
+          id: place.place_id
+        };
+        return marker;
+      });
+    }
+
+    /*
+    * markersBoundingBox: Fit map visualization to the current markers list
+    * */
+    function markersBoundingBox(locations) {
+      uiGmapGoogleMapApi.then(function(maps) {
+        var bounds = new maps.LatLngBounds();
+        _.each(locations, function (location) {
+          var place = location.results[0];
+          bounds.extend(place.geometry.location);
+        });
+        vm.map.getGMap().fitBounds(bounds);
+      });
+    }
+
     initMap();
+    loadDirections();
   }
 })();
---
+++ src/app/main/landing/landing.service.js
@@ -0,0 +1,53 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('App')
+    .service('LandingService', LandingService);
+
+  /** @ngInject */
+  function LandingService($resource, $q, _, Google_API_Key) {
+
+    /*
+    * Services:
+    *   getDirectionsData: Load a list of directions
+    *   geocodeAddress: Transform a raw address to coordinates
+    *   geocodeAddressList: Transform a list of raw address to a list of coordinates
+    * */
+    var service = {
+      getDirectionsData: getDirectionsData,
+      geocodeAddress: geocodeAddress,
+      geocodeAddressList: geocodeAddressList
+    };
+
+    /*
+    * Resources
+    *   - directions: loads plain text file "addresses.dat"
+    *   - geocode: Access to Google Geocode API
+    * */
+    var resources = {
+      directions: $resource("./assets/data/addresses.dat", {}, {'getText': {
+        transformResponse: function(data) {
+          var transformData = _.filter(data.split("\n"), _.identity);
+          return {content: transformData};
+        }
+      }}),
+      geocode: $resource("https://maps.googleapis.com/maps/api/geocode/json", {key: Google_API_Key})
+    };
+
+    return service;
+
+    function getDirectionsData() {
+      return resources.directions.getText().$promise;
+    }
+
+    function geocodeAddress(address) {
+      return resources.geocode.get({address: address}).$promise;
+    }
+
+    function geocodeAddressList(addressList) {
+      var promises = _.map(addressList, geocodeAddress);
+      return $q.all(promises);
+    }
+  }
+})();
